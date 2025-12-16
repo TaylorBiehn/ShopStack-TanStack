9ff6ec7b0db0d5f857c7c2ecc19785de2ee2b2f2
Taylor Biehn <taylorbiehn@outlook.com>
2025-12-16 20:59:40 +0600

    feat: Add new UI components for homepage hero section and integrate into store layout.

--- src/routes/(store)/_layout/index.tsx
+++ src/routes/(store)/_layout/index.tsx
@@ -1,7 +1,12 @@
+import Hero from "@/components/templates/store/homepage/heor";
 import { createFileRoute } from "@tanstack/react-router";
 
 export const Route = createFileRoute("/(store)/_layout/")({ component: App });
 
 function App() {
-  return <div>Hello</div>;
+  return (
+    <div className="min-h-screen">
+      <Hero />
+    </div>
+  );
 }
